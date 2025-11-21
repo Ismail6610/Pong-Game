e30777a80c23a22f395960bbdd1953bcc0b35f90
Ismail Bajrovic <ismailbajrovic2010@gmail.com>
2025-11-21 22:28:07 +0100

    Step 5: Ball and movement

--- PongGame/Game1.cs
+++ PongGame/Game1.cs
@@ -21,6 +21,13 @@ namespace PongGame
         float player2width = 20f;
         float player2height = 100f;
 
+        Texture2D ballTexture;
+        Vector2 ballPosition;
+        float ballSize = 20f;
+        Vector2 ballVelocity = new Vector2(300f, 300f); // X and Y speeds
+
+
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -45,9 +52,15 @@ namespace PongGame
             player1Texture.SetData(new[] { Color.White });
             player1Position = new Vector2(100,600);
 
+
             player2Texture = new Texture2D(GraphicsDevice, 1, 1);
             player2Texture.SetData(new[] { Color.White });
             player2Position = new Vector2(1100, 600);
+
+
+            ballTexture = new Texture2D(GraphicsDevice, 1, 1);
+            ballTexture.SetData(new[] { Color.White }); 
+            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2 - ballSize / 2, _graphics.PreferredBackBufferHeight / 2 - ballSize / 2);
 
         }
 
@@ -79,6 +92,25 @@ namespace PongGame
             player1Position.Y = Math.Clamp(player1Position.Y, 0, _graphics.PreferredBackBufferHeight - player1height);
             player2Position.Y = Math.Clamp(player2Position.Y, 0, _graphics.PreferredBackBufferHeight - player2height);
 
+            ballPosition += ballVelocity* deltaTime;
+
+            Rectangle ballRect = new Rectangle((int)ballPosition.X, (int)ballPosition.Y, (int)ballSize, (int)ballSize);
+            Rectangle player1Rect = new Rectangle((int)player1Position.X, (int)player1Position.Y, (int)player1width, (int)player1height);
+            Rectangle player2Rect = new Rectangle((int)player2Position.X, (int)player2Position.Y, (int)player2width, (int)player2height);
+
+            if(ballRect.Intersects(player1Rect) || ballRect.Intersects(player2Rect))
+            {
+                ballVelocity.X = -ballVelocity.X;
+            }
+
+            if (ballPosition.Y <= 0 || ballPosition.Y + ballSize >= _graphics.PreferredBackBufferHeight)
+            {
+                ballVelocity.Y *= -1; 
+            }
+
+
+
+
             base.Update(gameTime);
         }
 
@@ -86,11 +118,14 @@ namespace PongGame
         {
             Rectangle player1Rect = new Rectangle((int)player1Position.X, (int)player1Position.Y, (int)player1width, (int)player1height);
             Rectangle player2Rect = new Rectangle((int)player2Position.X, (int)player2Position.Y, (int)player2width, (int)player2height);
+            Rectangle ballRect = new Rectangle((int)ballPosition.X, (int)ballPosition.Y, (int)ballSize, (int)ballSize);
+
 
             GraphicsDevice.Clear(new Color(34,34,34));
             _spriteBatch.Begin();
             _spriteBatch.Draw(player1Texture, player1Rect, Color.White);
             _spriteBatch.Draw(player2Texture, player2Rect, Color.White);
+            _spriteBatch.Draw(ballTexture, ballRect, Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
