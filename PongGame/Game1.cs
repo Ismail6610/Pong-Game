128938fd5a84710d0cb3227bd8e126cf17956a39
Ismail Bajrovic <ismailbajrovic2010@gmail.com>
2025-11-21 22:05:38 +0100

    Step 3: Create the paddles

--- PongGame/Game1.cs
+++ PongGame/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SharpDX.Win32;
 
 namespace PongGame
 {
@@ -8,20 +9,30 @@ namespace PongGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-       
+
+        Texture2D player1Texture;
+        Vector2 player1Position;
+        float player1width = 20f;
+        float player1height = 100f;
+
+        Texture2D player2Texture;
+        Vector2 player2Position;
+        float player2width = 20f;
+        float player2height = 100f;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            _graphics.PreferredBackBufferWidth = 800;  
-            _graphics.PreferredBackBufferHeight = 600; 
+            int screenWidth = 1200;
+            _graphics.PreferredBackBufferWidth = screenWidth;  
+            _graphics.PreferredBackBufferHeight = 1000; 
             _graphics.ApplyChanges();
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
 
             base.Initialize();
         }
@@ -29,8 +40,14 @@ namespace PongGame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            player1Texture = new Texture2D(GraphicsDevice, 1,1);
+            player1Texture.SetData(new[] { Color.White });
+            player1Position = new Vector2(100,600);
 
-            // TODO: use this.Content to load your game content here
+            player2Texture = new Texture2D(GraphicsDevice, 1, 1);
+            player2Texture.SetData(new[] { Color.White });
+            player2Position = new Vector2(1100, 600);
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -38,15 +55,19 @@ namespace PongGame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(new Color(34,34,34));
+            Rectangle player1Rect = new Rectangle((int)player1Position.X, (int)player1Position.Y, (int)player1width, (int)player1height);
+            Rectangle player2Rect = new Rectangle((int)player2Position.X, (int)player2Position.Y, (int)player2width, (int)player2height);
 
+            GraphicsDevice.Clear(new Color(34,34,34));
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(player1Texture, player1Rect, Color.White);
+            _spriteBatch.Draw(player2Texture, player2Rect, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
